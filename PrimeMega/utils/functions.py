08c4b7fe8731d2432d2c12d1e87265678880043a
Xayzo <deonseorang@gmail.com>
2022-03-17 13:53:32 +0700

    Update functions.py

--- PrimeMega/utils/functions.py
+++ PrimeMega/utils/functions.py
@@ -91,14 +91,6 @@ async def get_http_status_code(url: str) -> int:
         return resp.status
 
 
-async def make_carbon(code):
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiosession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
-    image.name = "carbon.png"
-    return image
-
-
 async def transfer_sh(file_or_message):
     if isinstance(file_or_message, Message):
         file_or_message = await file_or_message.download()
